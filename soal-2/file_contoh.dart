2c44b699aec36e4d30fbde8729b5b5c220a36157
Ulfa-Ladayya <ladayaulfa@upi.edu>
2023-02-18 13:45:08 +0700

    File Percontohan untuk Command Push

---
+++ soal-2/file_contoh.dart
@@ -0,0 +1,3 @@
+void main(){
+   print('hanya file percontohan untuk soal prioritas 2 section 5'); 
+}
---
+++ soal-2/file_contoh1.dart
@@ -0,0 +1,5 @@
+void main(){
+   var judul = "Contoh File1";
+   print('Judulnya adalah $judul');
+   print('hanya file percontohan untuk soal prioritas 2 section 5'); 
+}
