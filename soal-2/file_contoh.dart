53a7a119e65ca4ade122af30a605ec35ab27cecb
Ulfa-Ladayya <ladayaulfa@upi.edu>
2023-02-18 14:03:18 +0700

    Memperbaiki line 3

--- soal-2/file_contoh.dart
+++ soal-2/file_contoh.dart
@@ -1,3 +1,4 @@
 void main(){
-   print('hanya file percontohan untuk soal prioritas 2 section 5'); 
+   print('hanya file percontohan untuk soal prioritas 2 section 5');
+   print('soal prioritas 2 section 5'); 
 }
